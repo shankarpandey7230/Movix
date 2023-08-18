f265a1f4d38dbe896abaf279ca337cd08c609b7f
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-08-19 02:55:06 +1000

    TMDB API Setup - API Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
 function App() {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((response) => {
+      console.log(response);
+    });
+  };
   return <div className="App">App</div>;
 }
 
