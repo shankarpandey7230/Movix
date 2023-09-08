a3466b5e87771bd3b220ec9a25b01fa2ad4ed4f8
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-09-09 05:32:54 +1000

    search result pages

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from "react";
-import "./style.scss";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import useFetch from "../../../hooks/useFetch";
 import { useSelector } from "react-redux";
+import "./style.scss";
+
+import useFetch from "../../../hooks/useFetch";
+
 import Img from "../../../components/lazyLoadImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
@@ -18,29 +21,32 @@ const HeroBanner = () => {
       data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
     setBackground(bg);
   }, [data]);
+
   const searchQueryHandler = (event) => {
-    if (event.key == "Enter" && query.length > 0) {
+    if (event.key === "Enter" && query.length > 0) {
       navigate(`/search/${query}`);
     }
   };
+
   return (
     <div className="heroBanner">
       {!loading && (
-        <div className="backdrop_img">
+        <div className="backdrop-img">
           <Img src={background} />
         </div>
       )}
+
       <div className="opacity-layer"></div>
       <ContentWrapper>
         <div className="heroBannerContent">
-          <span className="title">Welcome</span>
+          <span className="title">Welcome.</span>
           <span className="subTitle">
-            Millions of movies,TV shows and people to discover.Explore now.
+            Millions of movies, TV shows and people to discover. Explore now.
           </span>
           <div className="searchInput">
             <input
               type="text"
-              placeholder="Search for a movie or tv show..."
+              placeholder="Search for a movie or tv show...."
               onChange={(e) => setQuery(e.target.value)}
               onKeyUp={searchQueryHandler}
             />
