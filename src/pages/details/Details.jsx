de1028f388d8e556facd1859e2f995b2b0992231
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-09-09 02:31:12 +1000

    Videos sections of details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,6 +4,7 @@ import "./style.scss";
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
 const Details = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
@@ -16,6 +17,7 @@ const Details = () => {
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
     </div>
   );
 };
