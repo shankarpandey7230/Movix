594f2210354a40b01a8527ab1603f20fd56b737f
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-09-11 12:37:57 +1000

    fixing bugs for deployment

--- src/pages/details/videosSection/VideosSection.jsx
+++ src/pages/details/videosSection/VideosSection.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import "./style.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import { PlayIcon } from "../playIcon";
+import { PlayIcon } from "../PlayIcon";
 import VideoPopup from "../../../components/videoPopup/VideoPopup";
 import Img from "../../../components/lazyLoadImage/Img";
 
